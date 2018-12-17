ca3c0a2df755aa1ba3426696830c304674861e3a
Yauheni Lychkouski <bredobulbulizator@gmail.com>
2018-12-18 02:07:40 +0300

    Initial project setup

---
+++ Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  life-client
+//
+//  Created by Yauheni Lychkouski on 12/18/18.
+//  Copyright © 2018 Yauheni Lychkouski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
