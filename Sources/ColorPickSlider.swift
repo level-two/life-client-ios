9418c930f148e7d951bb66c60b0947b2dd711324
Yauheni Lychkouski <bredobulbulizator@gmail.com>
2018-12-22 23:48:51 +0300

    Slider drawing and updating implementation.

--- Sources/ColorPickSlider.swift
+++ Sources/ColorPickSlider.swift
@@ -11,15 +11,26 @@ import UIKit
 import QuartzCore
 
 class ColorPickSlider: UIControl {
-    let trackLayer = CALayer()
+    let trackLayer = ColorPickSliderTrackLayer()
     let thumbLayer = ColorPickSliderThumbLayer()
     var thumbSize: CGFloat {
         return CGFloat(bounds.height)
     }
+    var curvaceousness: CGFloat = 1.0 {
+        didSet {
+            trackLayer.setNeedsDisplay()
+            thumbLayer.setNeedsDisplay()
+        }
+    }
     
     let minimumValue = 0.0
     let maximumValue = 1.0
-    var value = 0.0
+    var value = 0.0  {
+        didSet {
+            updateLayerFrames()
+            print(value)
+        }
+    }
     
     var previousLocation = CGPoint()
     
@@ -34,11 +45,14 @@ class ColorPickSlider: UIControl {
     }
     
     func addSublayers() {
-        trackLayer.backgroundColor = UIColor.blue.cgColor
+        // trackLayer.backgroundColor = UIColor.blue.cgColor
+        trackLayer.colorPickSlider = self
+        trackLayer.contentsScale = UIScreen.main.scale
         layer.addSublayer(trackLayer)
         
-        thumbLayer.backgroundColor = UIColor.green.cgColor
+        // thumbLayer.backgroundColor = UIColor.green.cgColor
         thumbLayer.colorPickSlider = self
+        thumbLayer.contentsScale = UIScreen.main.scale
         layer.addSublayer(thumbLayer)
         
         updateLayerFrames()
@@ -51,13 +65,18 @@ class ColorPickSlider: UIControl {
     }
     
     func updateLayerFrames() {
-        trackLayer.frame = bounds.insetBy(dx: 0.0, dy: bounds.height / 3)
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        
+        trackLayer.frame = bounds.insetBy(dx: thumbSize/2.0, dy: bounds.height/3.0)
         trackLayer.setNeedsDisplay()
         
         let thumbCenter = CGFloat(positionForValue(value))
         thumbLayer.frame = CGRect(x: thumbCenter - thumbSize / 2.0, y: 0.0,
                                   width: thumbSize, height: thumbSize)
         thumbLayer.setNeedsDisplay()
+        
+        CATransaction.commit()
     }
     
     func positionForValue(_ value: Double) -> Double {
@@ -66,7 +85,7 @@ class ColorPickSlider: UIControl {
     }
     
     override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
-        previousLocation = touch.location(in: self)
+        previousLocation = touch.location(in: self).clamped(to: bounds)
         if thumbLayer.frame.contains(previousLocation) {
             thumbLayer.highlighted = true
         }
@@ -80,20 +99,14 @@ class ColorPickSlider: UIControl {
         let deltaLocation = Double(location.x - previousLocation.x)
         let deltaValue = (maximumValue - minimumValue) * deltaLocation / Double(bounds.width - thumbSize)
         
-        previousLocation = location.clamped(to: bounds)
+        previousLocation = location.clamped(to: bounds.insetBy(dx: thumbSize/2.0, dy: thumbSize/2.0))
         
         // 2. Update the values
         if thumbLayer.highlighted {
-            value += deltaValue
-            value.clamp(minimumValue, maximumValue)
+            value = (value + deltaValue).clamped(minimumValue, maximumValue)
         }
         
-        // 3. Update the UI
-        CATransaction.begin()
-        CATransaction.setDisableActions(true)
-        updateLayerFrames()
-        CATransaction.commit()
-        
+        sendActions(for: .valueChanged)
         return true
     }
     
@@ -105,10 +118,6 @@ class ColorPickSlider: UIControl {
 extension Double {
     func clamped(_ lower: Double, _ upper: Double) -> Double {
         return min(max(self, lower), upper)
-    }
-    
-    mutating func clamp(_ lower: Double, _ upper: Double) {
-        self = min(max(self, lower), upper)
     }
 }
 
@@ -127,6 +136,56 @@ extension CGPoint {
 }
 
 class ColorPickSliderThumbLayer: CALayer {
-    var highlighted = false
     weak var colorPickSlider: ColorPickSlider?
+    var highlighted = false {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    override func draw(in ctx: CGContext) {
+        guard let slider = colorPickSlider else { return }
+        
+        let thumbFrame = bounds.insetBy(dx: 2.0, dy: 2.0)
+        let cornerRadius = thumbFrame.height * slider.curvaceousness / 2.0
+        let thumbPath = UIBezierPath(roundedRect: thumbFrame, cornerRadius: cornerRadius).cgPath
+        
+        // Fill - with a subtle shadow
+        let shadowColor = UIColor.gray.cgColor
+        
+        ctx.setShadow(offset: CGSize(width: 0.0, height: 1.0), blur: 1.0, color: shadowColor)
+        ctx.setFillColor(UIColor.red.cgColor)
+        ctx.addPath(thumbPath)
+        ctx.fillPath()
+        
+        // Outline
+        ctx.setStrokeColor(shadowColor)
+        ctx.setLineWidth(0.5)
+        ctx.addPath(thumbPath)
+        ctx.strokePath()
+        
+        if highlighted {
+            ctx.setFillColor(UIColor(white: 0.0, alpha: 0.1).cgColor)
+            ctx.addPath(thumbPath)
+            ctx.fillPath()
+        }
+    }
+}
+
+class ColorPickSliderTrackLayer: CALayer {
+    weak var colorPickSlider: ColorPickSlider?
+    
+    override func draw(in ctx: CGContext) {
+        guard let slider = colorPickSlider else { return }
+        
+        // Clip
+        let cornerRadius = bounds.height * slider.curvaceousness / 2.0
+        let path = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius).cgPath
+        ctx.addPath(path)
+        
+        // Fill the track
+        ctx.setFillColor(UIColor.green.cgColor)
+        ctx.addPath(path)
+        ctx.fillPath()
+    }
 }
