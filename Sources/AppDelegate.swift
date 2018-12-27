870df61c74948b7e15eb4c19ac22f3e50809c5ee
Yauheni Lychkouski <bredobulbulizator@gmail.com>
2018-12-27 16:35:57 +0300

    Hide navigation bar

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -24,6 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let navigationController = UINavigationController()
+        navigationController.isNavigationBarHidden = true
         let container = DependencyContainer(navigationController: navigationController)
         let loginViewController = container.makeLoginViewController()
         navigationController.viewControllers = [loginViewController]
